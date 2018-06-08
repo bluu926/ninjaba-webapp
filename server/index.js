6d127f7e8b9ffc2a2b49c7a0e016789f41c8a2d1
bluu926 <bluumail@gmail.com>
2018-06-08 00:11:52 -0700

    adding server initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+
+// App Setup
+app.use(morgan('combined')); // Middleware in express
+app.use(bodyParser.json({ type: '*/*'})); // Middleware in express
+router(app);
+
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port)
