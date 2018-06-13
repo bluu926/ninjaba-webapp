aa97c3921624e7ff645d65a6c36d5b90acec431d
bluu926 <bluumail@gmail.com>
2018-06-12 22:58:58 -0700

    adding signin route

--- server/services/passport.js
+++ server/services/passport.js
@@ -2,17 +2,42 @@ const passport = require('passport');
 const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = rquire('passport-jwt').ExtractJwt;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local Strategy
+const localOptions = { usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions , function (email, password, done) {
+  // Verify this email and passowrd, call done with the user
+  // if it is correct email and password
+  // otherwise, call done with false
+  User.findOne({ email: email }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false); }
+
+    // compare passwords - is 'password' equal to user.password?
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false); }
+
+      return done(null, user);
+    });
+  });
+});
+
 
 // Setup options for JWT Strategy
-const jwtOptions = {};
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
 
 // Create JWT Strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
   // See if the user ID in the payload exits in our database
   // If it does, call 'done' with that user
   // otherwise, call done without a user object
-  User.findByID(payload.sub, function (err, user) {
+  User.findById(payload.sub, function (err, user) {
     if (err) { return done(err, false); }
 
     if (user) {
@@ -24,3 +49,5 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 // Tell pasport to use this strategy
+passport.use(jwtLogin);
+passport.use(localLogin);
