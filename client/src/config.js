d7e74d9e4ce55bff3c05b702dfa1d1deff21674c
bluu926 <bluumail@gmail.com>
2018-07-03 00:44:26 -0700

    change servers

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://ninjaba.herokuapp.com/api";
-export const API_URL = 'http://localhost:3090/api';
+export const API_URL = "https://ninjaba.herokuapp.com/api";
+// export const API_URL = 'http://localhost:3090/api';
